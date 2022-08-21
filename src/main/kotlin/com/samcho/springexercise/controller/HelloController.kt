d119d22efeba8279e5efd8de36e2da763097a588
Sam Cho <organicyellow0608@gmail.com>
2022-08-21 17:17:18 +0900

    <FEAT> learned the basic use of thymeleaf

--- src/main/kotlin/com/samcho/springexercise/controller/HelloController.kt
+++ src/main/kotlin/com/samcho/springexercise/controller/HelloController.kt
@@ -3,6 +3,8 @@ package com.samcho.springexercise.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller // when using a controller, the returned value is used by `viewResolver` to find the screen to respond with
 class HelloController {
@@ -12,5 +14,31 @@ class HelloController {
         model.addAttribute("data", "hello!!")
         return "hello" // name of the template html file the model will be passed to
         // find `resources:templates/{VIEW_NAME}.html
+    }
+
+    @GetMapping("hello-mvc")
+    fun helloMvc(@RequestParam("name") name: String, model: Model) : String {
+        model.addAttribute("name", name)
+        return "hello-template"
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // inserts the returned value to the response body
+    fun helloString(@RequestParam("name") name: String,) : String {
+        return "hello $name"
+    }
+
+    companion object {
+        class Hello {
+            var name: String? = null
+        }
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    fun helloApi(@RequestParam("name") name: String) : Hello {
+        return Hello().apply {
+            this.name = name
+        }
     }
 }
